d50df7b69caeecafa24b0682423e2cbf0640d736
Mickael <mickael.bettinelli@protonmail.com>
2022-05-20 09:57:54 +0300

    test changing the training method

--- train_model/train.py
+++ train_model/train.py
@@ -22,7 +22,16 @@ def _train_model(x_train, y_train):
 
 
 if __name__ == '__main__':
-    mlflow.set_tracking_uri("http://0.0.0.0:8000")
+    model = pickle.load(open(model_name, 'rb'))
+    #mlflow.set_tracking_uri("http://127.0.0.1:8000")
+    print("CACAO")
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--x_train')
+    parser.add_argument('--y_train')
+    args = parser.parse_args()
+    _train_model(args.x_train, args.y_train)
+    """
+    mlflow.set_tracking_uri("file:///tmp/my_tracking")
     mlflow.set_experiment("train-model")
 
     parser = argparse.ArgumentParser()
@@ -37,3 +46,4 @@ if __name__ == '__main__':
     mlflow.sklearn.log_model(model, "sk_learn",
                              serialization_format="cloudpickle",
                              registered_model_name="sklearn-grid-search-cv-model")
+    """
